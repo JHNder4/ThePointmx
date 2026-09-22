b3ad195fd40f96ab6cbdf2e5594cb69fcacdc355
JHNder4 <2.84304774e+08+JHNder4@users.noreply.github.com>
2026-09-22 08:06:48 +0000

    Corregir carga del admin y limpiar dependencias

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import obfuscator from 'vite-plugin-javascript-obfuscator'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    obfuscator({options:{compact:true,controlFlowFlattening:true,stringArray:true,stringArrayEncoding:['base64'],deadCodeInjection:true}}),
   ],
   resolve: {
     alias: {
